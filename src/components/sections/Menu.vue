0162c8625aecb350717dcf2b66c85cb41f0b7ca1
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-12-13 16:40:13 -0300

    Adds new icons to Menu section

--- src/components/sections/Menu.vue
+++ src/components/sections/Menu.vue
@@ -1,73 +1,86 @@
 <template>
   <section>
     <a href="">
-      <img src='../../assets/hearthstone-battlegrounds.png' alt="">
+      <img src="../../assets/hearthstone-battlegrounds.png" alt="" />
     </a>
-    <Button type='button'>
-      <i class="fas fa-plus"></i>
+    <Button type="button">
+      <PlusIcon />
       Add new entry
     </Button>
-    <div class='links'>
-      <Button type='link'>
-        <i class="fas fa-home"></i>
+    <div class="links">
+      <Button type="link">
+        <HomeIcon />
         Home
       </Button>
-      <Button type='link'>
-        <i class="far fa-chart-bar"></i>
+      <Button type="link">
+        <ChartIcon />
         Charts
       </Button>
-      <Button type='link'>
-        <i class="fas fa-table"></i>
+      <Button type="link">
+        <ListIcon />
         Registers
       </Button>
-      <Button type='link'>
-        <i class="fas fa-users"></i>
+      <Button type="link">
+        <PeopleIcon />
         Heroes
       </Button>
-      <Button type='link'>
-        <i class="fas fa-cogs"></i>
+      <Button type="link">
+        <SettingsIcon />
         Settings
       </Button>
-      <Button type='link'>
-        <i class="fas fa-sign-out-alt"></i>
+      <Button type="link">
+        <LogoutIcon />
         Sign out
       </Button>
-      
     </div>
   </section>
 </template>
 
 <script>
-  import Button from '../elements/Button.vue';
+import Button from "../elements/Button.vue";
+import HomeIcon from "../icons/Home.vue";
+import ChartIcon from "../icons/Chart.vue";
+import ListIcon from "../icons/List.vue";
+import PeopleIcon from "../icons/People.vue";
+import SettingsIcon from "../icons/Settings.vue";
+import LogoutIcon from "../icons/Logout.vue";
+import PlusIcon from "../icons/Plus.vue";
 
-  export default {
-    name: 'Menu',
-    components: {
-      Button,
-    }
-  }
+export default {
+  name: "Menu",
+  components: {
+    Button,
+    HomeIcon,
+    ChartIcon,
+    ListIcon,
+    PeopleIcon,
+    SettingsIcon,
+    LogoutIcon,
+    PlusIcon,
+  },
+};
 </script>
 
 <style scoped>
-  section {
-    position: relative;
-    height: 100%;
-    width: var(--menu-width);
-    background-color: var(--white-colour);
-    padding-top: 20px;
-    padding-left: 30px;
-    padding-right: 30px;
-  }
+section {
+  position: relative;
+  height: 100%;
+  width: var(--menu-width);
+  background-color: var(--white-colour);
+  padding-top: 20px;
+  padding-left: 30px;
+  padding-right: 30px;
+}
 
-  section img {
-    width: 80%;
-    margin: 0px 10% 40px;
-  }
+section img {
+  width: 80%;
+  margin: 0px 10% 40px;
+}
 
-  section .links {
-    margin-top: 40px;
-  }
-  section .links button {
-    margin-bottom: 10px;
-  }
+section .links {
+  margin-top: 40px;
+}
+section .links button {
+  margin-bottom: 10px;
+}
 </style>
